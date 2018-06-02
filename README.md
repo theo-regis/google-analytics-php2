614169724b809d8fe32acf1ae4b843a3e9b4fac1
theo-regis <37154327+theo-regis@users.noreply.github.com>
2018-06-02 18:01:28 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
-# google-analytics-php2
+# Google Analytics Beacon ![analytics image (flat)](https://raw.githubusercontent.com/vitr/google-analytics-beacon/master/static/badge-flat.gif)[![Analytics](https://vitr-analytics.appspot.com/UA-75628680-1/google-analytics-beacon?pixel&useReferer)](https://github.com/vitr/google-analytics-beacon)  
+Sometimes it is impossible to embed the JavaScript tracking code provided by Google Analytics: the host page does not allow arbitrary JavaScript, and there is no Google Analytics integration. However, not all is lost! **If you can embed a simple image (pixel tracker), then you can beacon data to Google Analytics.**
+
+---------------
+This is tweaked version of [igrigorik/ga-beacon](https://github.com/igrigorik/ga-beacon).
+As the original author suggests:
+>there are no capacity or availability promises. For best results, deploy your own instance directly on Google App Engine.
+
+Unfortunately, many people don't bother with creating  their own instances and we all see this
+![over quota error image](https://github.com/vitr/vitr.github.io/blob/master/_posts/Image%201.png?raw=true "Over Quota")
+
+This repo was created to help you with setting up your own Google Analytis Beacon application, so, you will never have availabiltiy issues like above.
+
+Its usage is also restricted by your onw tracking ids (`UA-XXXXXXXX-X`). The file `ga-beacon/conf.json` contains a white list of allowed tracking ids. This implies, you have to update the list every time you want to track something with a new tracking id. Leave the list empty, if you don't want to restrict, but keep in mind, with no restrictions anyone can use your application instance for tracking their visitors and you may pay for that. Here an example of `ga-beacon/conf.json`
+
+    {
+        "TrackingIds": ["UA-XXXXXXXX-X","UA-YYYYYYYY-Y,"UA-ZZZZZZZZ-Z"]
+    }
+
+You may also auto-calculate the tracking path based in the "referer" information of the image. To activate this simple add `?useReferrer` to the image URL (or `&useReferer` if you need to combine this with the `?pixel`, `?flat` or `?flat-gif` parameter). Although they are some odd browsers that don't always send the referer header, the amount of traffic coming from those browsers is usually not relevant at all. Of course that if you need to measure the traffic from those odd browsers you should not use this method.
+
+### How to deploy on Google App Engine
+You should deploy this application on your server. Using Google App Engine is free and easiest way to do so. Be aware of GAE free usage limits https://cloud.google.com/appengine/docs/quotas#Requests. If you run over quota, you'll see the image above and lose some stats. Unless you have very popular resource or other people also use your instance for tracking (consider applying the restriction), GAE daily limits are very generous. There are two options for deployment on GAE:
+
+#### Using Google Developers Console (easy)
+
+1. Fork this repo
+2. Go to https://console.cloud.google.com/
+3. Create a project **My-Project**
+4. Go to Development->Source code and connet the forked repo
+5. Activate Google Cloud Shell and run the following commands
+6. `cd ~/src/My-Project-ID/master`  
+7. `gcloud preview app deploy app.yaml --promote`
+8. Confirm and wait for the deployment success message
+8. The app is running now! You can check the badge at https://My-Project-ID.appspot.com/UA-XXXXXXXX-X/your-page and start using it. See [how to setup Google Analytics](#how-to-setup-google-analytics)
+
+
+
+
+
+#### Using Google App Engine SDK for Go (advanced)
+https://cloud.google.com/appengine/downloads#Google_App_Engine_SDK_for_Go
+
+[Hello, World! in 5 minutes - Go — Google Cloud Platform](https://cloud.google.com/appengine/docs/go/)
+
+http://stackoverflow.com/questions/32107712/google-app-engine-app-deployment
+
+In my case, I got refused because the appcfg save my ouauth2 token in the file ~/.appcfg_oauth2_tokens, which happen to be another account of appengine . Simply remove the file and try again.
+
+`cd $GOPATH` or `cd %GOPATH%` on win    
+`appcfg.py -A your_app_name update google-analytics-beacon/`
+
+### How to setup Google Analytics
+
+First, log in to your Google Analytics account and [set up a new property](https://support.google.com/analytics/answer/1042508?hl=en):
+
+* Select "Website", use new "Universal Analytics" tracking
+* **Website name:** anything you want (e.g. GitHub projects)
+* **WebSite URL: https://My-Project-ID.appspot.com/**
+* Click "Get Tracking ID", copy the `UA-XXXXXXXX-X` ID on next page
+
+Next, add a tracking image to the pages you want to track:
+
+* _https://My-Project-ID.appspot.com/UA-XXXXXXXX-X/insert/any/path_
+* `UA-XXXXX-X` should be your tracking ID
+* `insert/any/path` is an arbitrary path. For best results specify a meaningful and self-descriptive path. You have to do this manually, the beacon won't automatically record the page path it's embedded on.
+
+Example tracker markup if you are using Markdown:
+
+```markdown
+[![Analytics](https://My-Project-ID.appspot.com/UA-XXXXXXXX-X/insert/any/path)](https://github.com/vitr/google-analytics-beacon)
+```
+
+If you prefer, you can skip the badge and use a transparent pixel. To do so, simply append `?pixel` to the image URL. There are also "flat" style variants available, which are available when appending `?flat` or `?flat-gif` to the image URL. And that's it, add the tracker image to the pages you want to track and then head to your Google Analytics account to see real-time and aggregated visit analytics for your projects!
+
+It's useful to read the original project [FAQ](https://github.com/igrigorik/ga-beacon#faq)
+
+### Roadmap
+- [ ] Add referer option from https://github.com/igrigorik/ga-beacon/commit/dd96cd440b98435b9e03d681f8e0e248328026a9
+- [x] Modify original Go program to optionally restrict usage by tracking id
+- [ ] Add GAE deployment help
+- [ ] Add setup help (update original help with recent workflow)
+- [ ] Deploy on AWS Elastic Beanstalk
+- [ ] Create docker image for universal deployment
---
+++ ga-beacon/ga-beacon.go
@@ -0,0 +1,286 @@
+package beacon
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"appengine"
+	"appengine/delay"
+	"appengine/urlfetch"
+
+	"encoding/json"
+	"os"
+)
+
+const beaconURL = "http://www.google-analytics.com/collect"
+
+var (
+	pixel        = mustReadFile("static/pixel.gif")
+	badge        = mustReadFile("static/badge.svg")
+	badgeGif     = mustReadFile("static/badge.gif")
+	badgeFlat    = mustReadFile("static/badge-flat.svg")
+	badgeFlatGif = mustReadFile("static/badge-flat.gif")
+	pageTemplate = template.Must(template.New("page").ParseFiles("ga-beacon/page.html"))
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func mustReadFile(path string) []byte {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+func generateUUID(cid *string) error {
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		return err
+	}
+
+	b[8] = (b[8] | 0x80) & 0xBF // what's the purpose ?
+	b[6] = (b[6] | 0x40) & 0x4F // what's the purpose ?
+	*cid = hex.EncodeToString(b)
+	return nil
+}
+
+var delayHit = delay.Func("collect", logHit)
+
+func log(c appengine.Context, ua string, ip string, cid string, values url.Values) error {
+	req, _ := http.NewRequest("POST", beaconURL, strings.NewReader(values.Encode()))
+	req.Header.Add("User-Agent", ua)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	if resp, err := urlfetch.Client(c).Do(req); err != nil {
+		c.Errorf("GA collector POST error: %s", err.Error())
+		return err
+	} else {
+		c.Debugf("GA collector status: %v, cid: %v, ip: %s", resp.Status, cid, ip)
+		c.Debugf("Reported: %v", values)
+	}
+	return nil
+}
+
+func postphp(php string, ua string, c appengine.Context) error {
+
+	req, _ := http.NewRequest("POST", php, nil)
+	req.Header.Add("User-Agent", ua)
+	req.Header.Add("Content-Type", "application/php")
+
+	if resp, err := urlfetch.Client(c).Do(req); err != nil {
+		c.Errorf("PHP Error: %s", err.Error())
+		return err
+	} else {
+		c.Debugf("PHP status: %v", resp.Status)
+		//c.Debugf("Reported: %v", values)
+	}
+	return nil
+
+}
+
+func logHit(c appengine.Context, params []string, query url.Values, ua string, ip string, cid string) error {
+	// 1) Initialize default values from path structure
+	// 2) Allow query param override to report arbitrary values to GA
+	//
+	// GA Protocol reference: https://developers.google.com/analytics/devguides/collection/protocol/v1/reference
+
+	payload := url.Values{
+		"v":   {"1"},        // protocol version = 1
+		"t":   {"pageview"}, // hit type
+		"tid": {params[0]},  // tracking / property ID
+		"cid": {cid},        // unique client ID (server generated UUID)
+		"dp":  {params[1]},  // page path
+		"uip": {ip},         // IP address of the user
+	}
+
+	for key, val := range query {
+		payload[key] = val
+	}
+
+	return log(c, ua, ip, cid, payload)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	params := strings.SplitN(strings.Trim(r.URL.Path, "/"), "/", 2)
+	query, _ := url.ParseQuery(r.URL.RawQuery)
+	refOrg := r.Header.Get("Referer")
+
+	// / -> redirect
+	if len(params[0]) == 0 {
+		http.Redirect(w, r, "http://imailing.com.br/", http.StatusFound)
+		return
+	}
+
+	// check allowed Tracking Ids
+	config := readConfig("ga-beacon/conf.json")
+	if !checkTrackingId(params[0], config.TrackingIds) {
+		http.Error(w, "Forbidden, read http://imailing.com.br/", 403)
+		return
+	}
+
+	// activate referrer path if ?useReferer is used and if referer exists
+	if _, ok := query["useReferer"]; ok {
+		if len(refOrg) != 0 {
+			referer := strings.Replace(strings.Replace(refOrg, "http://", "", 1), "https://", "", 1);
+			if len(referer) != 0 {
+				// if the useReferer is present and the referer information exists
+				//  the path is ignored and the beacon referer information is used instead.
+				params = strings.SplitN(strings.Trim(r.URL.Path, "/") + "/" + referer, "/", 2)
+			}
+		}
+	}
+	// /account -> account template
+	if len(params) == 1 {
+		templateParams := struct {
+			Account string
+			Referer string
+		}{
+			Account: params[0],
+			Referer: refOrg,
+		}
+		if err := pageTemplate.ExecuteTemplate(w, "page.html", templateParams); err != nil {
+			http.Error(w, "could not show account page", 500)
+			c.Errorf("Cannot execute template: %v", err)
+		}
+		return
+	}
+
+	// /account/page -> GIF + log pageview to GA collector
+	var cid string
+	if cookie, err := r.Cookie("cid"); err != nil {
+		if err := generateUUID(&cid); err != nil {
+			c.Debugf("Failed to generate client UUID: %v", err)
+		} else {
+			c.Debugf("Generated new client UUID: %v", cid)
+			http.SetCookie(w, &http.Cookie{Name: "cid", Value: cid, Path: fmt.Sprint("/", params[0])})
+		}
+	} else {
+		cid = cookie.Value
+		c.Debugf("Existing CID found: %v", cid)
+	}
+
+	if len(cid) != 0 {
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("CID", cid)
+
+		logHit(c, params, query, r.Header.Get("User-Agent"), r.RemoteAddr, cid)
+		// delayHit.Call(c, params, r.Header.Get("User-Agent"), cid)
+	}
+	
+	//Check if "link" param is used and redirect to the link
+	if _, ok := query["link"]; ok {
+		link := query.Get("link")
+		link = strings.Replace(link,"[inter]","?",-1)
+		link = strings.Replace(link,"[ecom]","&",-1)
+		
+		if _, ok := query["adv"]; ok {
+
+			if _, ok := query["cmp"]; ok {
+			
+				if _, ok := query["dgt"]; ok {
+		
+		
+					adv := query.Get("adv")
+					cmp := query.Get("cmp")
+					dgt := query.Get("dgt")
+					php := "http://www.password.com.br/php/evt.php?adv=" + adv + "&cmp=" + cmp + "&lnk=" + params[1] + "&ipa="+ r.RemoteAddr + "&dgt=" + dgt
+								
+					postphp(php,r.Header.Get("User-Agent"),c)
+			
+
+		
+				} 
+		
+			} 
+
+		}
+			
+		http.Redirect(w, r, link, 301)
+		return
+	}
+	
+	// Write out GIF pixel or badge, based on presence of "pixel" param.
+
+	if _, ok := query["pixel"]; ok {
+		w.Header().Set("Content-Type", "image/gif")
+		w.Write(pixel)
+	} else if _, ok := query["gif"]; ok {
+		w.Header().Set("Content-Type", "image/gif")
+		w.Write(badgeGif)
+	} else if _, ok := query["flat"]; ok {
+		w.Header().Set("Content-Type", "image/svg+xml")
+		w.Write(badgeFlat)
+	} else if _, ok := query["flat-gif"]; ok {
+		w.Header().Set("Content-Type", "image/gif")
+		w.Write(badgeFlatGif)
+	} else {
+		w.Header().Set("Content-Type", "image/svg+xml")
+		w.Write(badge)
+	}
+	
+	
+	
+	if _, ok := query["adv"]; ok {
+
+		if _, ok := query["cmp"]; ok {
+			
+			if _, ok := query["dgt"]; ok {
+		
+		
+				adv := query.Get("adv")
+				cmp := query.Get("cmp")
+				dgt := query.Get("dgt")
+				
+				php := "http://www.password.com.br/php/apt.php?adv=" + adv + "&cmp=" + cmp + "&lnk=" + params[1] + "&ipa="+ r.RemoteAddr + "&dgt=" + dgt
+				
+
+				postphp(php,r.Header.Get("User-Agent"),c)
+				
+			} 
+		
+		} 
+
+	} 
+	
+	
+	
+	
+}
+
+type Configuration struct {
+	TrackingIds []string
+}
+
+func readConfig(path string) Configuration {
+	file, _ := os.Open(path)
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	return configuration
+}
+
+func checkTrackingId(trackingId string, allowed []string) bool {
+	if len(allowed) == 0 {
+		return true
+	}
+	for _, item := range allowed {
+		if item == trackingId {
+			return true
+		}
+	}
+	return false
+}

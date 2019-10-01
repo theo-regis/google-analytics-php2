a71c9793255552929357bba8f693b7d06439b75b
theo-regis <37154327+theo-regis@users.noreply.github.com>
2019-10-01 15:43:59 -0300

    Update ga-beacon.go

--- ga-beacon.go
+++ ga-beacon.go
@@ -1,9 +1,10 @@
-package beacon
+package main
 
 import (
 	"crypto/rand"
 	"encoding/hex"
 	"fmt"
+	"log"
 	"html/template"
 	"io/ioutil"
 	"net/http"
@@ -29,8 +30,18 @@ var (
 	pageTemplate = template.Must(template.New("page").ParseFiles("ga-beacon/page.html"))
 )
 
-func init() {
+func main() {
 	http.HandleFunc("/", handler)
+	// [START setting_port]
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	// [END setting_port]
 }
 
 func mustReadFile(path string) []byte {
